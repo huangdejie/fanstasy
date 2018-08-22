a0745df113963b8ecf782dfaf8516a12ed89f339
dejie.huang <dejie.huang@zhiren360.com>
2018-08-22 17:42:21 +0800

    新增用户查询信息

--- fanstasy-web/src/main/java/com/cashbang/fanstasy/controller/UserController.java
+++ fanstasy-web/src/main/java/com/cashbang/fanstasy/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.cashbang.fanstasy.controller;
 
+import com.cashbang.fanstasy.entity.UserEntity;
 import com.cashbang.fanstasy.request.UserRequest;
 import com.cashbang.fanstasy.response.Response;
 import com.cashbang.fanstasy.service.UserService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Created by huangdejie on 2018/8/20 0020.
  */
+@Api(description = "用户接口")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -27,6 +31,16 @@ public class UserController {
     public Response login(HttpServletRequest httpServletRequest, @RequestBody UserRequest userRequest){
         userService.login(userRequest.getLoginName(),userRequest.getPassword());
         Response response = new Response();
+        return response;
+    }
+
+    @ApiOperation(value = "获取用户信息",notes = "获取用户信息")
+    @RequestMapping(value = "/queryUserInfo",method = RequestMethod.GET)
+    public Response queryUserInfo(HttpServletRequest httpServletRequest){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("你好");
+        Response response = new Response();
+        response.setDetail(userEntity);
         return response;
     }
 
